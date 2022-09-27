3ade942cc0825b4f462116312ee7a30cfe7a8695
Rutenis Raila <rutenisraila@hotmail.com>
2022-09-27 15:44:55 +0200

    add create / delete functionality

--- components/presentationals/examples/ListItem/ListItem.tsx
+++ components/presentationals/examples/ListItem/ListItem.tsx
@@ -14,13 +14,13 @@ interface Props {
 
 function ListItem({ data: { title, description }, onDeleteClick }: Props) {
   return (
-    <li className="flex items-center justify-between w-full px-4 py-2 space-x-2 bg-blue-700 rounded-lg">
+    <li className="flex items-center justify-between w-full px-4 py-2 space-x-2 text-white bg-blue-700 rounded-lg">
       <div className="space-y-1">
         <h3 className="font-bold">{title}</h3>
         <p className="text-sm italic">{description}</p>
       </div>
       <IconButton size="large" onClick={onDeleteClick}>
-        <IconDisplay name={IconNames.Cross} className="dark:stroke-white" />
+        <IconDisplay name={IconNames.Cross} className="stroke-white" />
       </IconButton>
     </li>
   );
